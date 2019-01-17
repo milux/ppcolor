f0f61b7a9f5f54af3c3286278c5d0a6898e3c92f
Michael Lux <michi.lux@gmail.com>
2019-01-17 17:48:46 +0100

    Optimistic eps guessing and cluster median instead of average

--- src/main/kotlin/de/milux/ppcolor/ml/DBSCANExecutor.kt
+++ src/main/kotlin/de/milux/ppcolor/ml/DBSCANExecutor.kt
@@ -3,6 +3,7 @@ package de.milux.ppcolor.ml
 import de.milux.ppcolor.N_COLORS
 import org.apache.commons.math3.ml.clustering.Cluster
 import org.slf4j.LoggerFactory
+import kotlin.math.min
 
 class DBSCANExecutor(hueList: List<HuePoint>) {
     private val points: List<HuePoint> = hueList.sortedBy { it.hue }
@@ -18,32 +19,40 @@ class DBSCANExecutor(hueList: List<HuePoint>) {
         val hueClusters = findNClusters()?.map { cluster -> cluster.points.map { it.hue } }
         return hueClusters?.map { hueValues ->
             if (hueValues.max()!! - hueValues.min()!! > 0.5) {
-                val translatedHues = hueValues.map { if (it < 0.5) it + 1f else it  }
-                val average = translatedHues.average().toFloat()
-                if (average < 1f) average else average - 1f
+                val translatedHues = hueValues.map { if (it < 0.5) it + 1f else it  }.sorted()
+                val median = translatedHues[translatedHues.size / 2]
+                if (median < 1f) median else median - 1f
             } else {
-                hueValues.average().toFloat()
+                hueValues.sorted()[hueValues.size / 2]
             }
         }
     }
 
-    private fun findNClusters(): List<Cluster<HuePoint>>? {
+    private fun findNClusters(optimistic: Boolean = true): List<Cluster<HuePoint>>? {
         var clusters = emptyList<Cluster<HuePoint>>()
         // Use bisection to refine number of clusters until N_COLORS clusters are found
         var runs = 0
-        var eps = MAX_EPS
+        var eps = optimisticEps
         while (eps > .001) {
-            clusters = getClusters(eps)
             runs++
+            clusters = getClusters(eps)
             if (clusters.size >= N_COLORS) {
-                logger.trace("Found $N_COLORS clusters after $runs iterations with eps = $eps")
-                return clusters.sortedByDescending { it.points.size }
+                optimisticEps = min(eps * OPTIMISTIC_EPS_FACTOR, MAX_EPS)
+                logger.trace("Found ${clusters.size} clusters after $runs iterations with eps = $eps")
+                return clusters
             }
             eps *= EPS_ANNEALING
         }
         if (clusters.size < N_COLORS) {
-            logger.warn("Found only ${clusters.size} clusters, skipping")
-            return null
+            // If this may have happened due to an optimistic execution, retry with conservative eps
+            return if (optimistic) {
+                logger.warn("Found only ${clusters.size} clusters (optimistic), retrying with MAX_EPS")
+                optimisticEps = MAX_EPS
+                findNClusters(false)
+            } else {
+                logger.warn("Found only ${clusters.size} clusters, skipping")
+                null
+            }
         }
         return clusters
     }
@@ -57,5 +66,8 @@ class DBSCANExecutor(hueList: List<HuePoint>) {
         const val EPS_ANNEALING = .8
         const val MAX_EPS = .1
         const val MIN_PTS_FACTOR = .1
+        const val OPTIMISTIC_EPS_FACTOR = 2.5
+
+        private var optimisticEps = MAX_EPS
     }
 }
